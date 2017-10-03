d7979c6458427005d9ca8395c00f94c0bf0ae1e7
DarkAtra <tommy@darkatra.de>
2017-10-04 01:28:13 +0200

    replaced @Component with @Configuration

--- src/main/java/de/darkatra/patcher/config/Config.java
+++ src/main/java/de/darkatra/patcher/config/Config.java
@@ -2,10 +2,10 @@ package de.darkatra.patcher.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Data
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "patcherConfig")
 public class Config {
 	private String serverUrl;
