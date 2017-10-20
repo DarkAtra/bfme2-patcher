e38ee9300bf439968b864660c80775ae297abcf9
DarkAtra <tommy@darkatra.de>
2017-10-20 17:34:28 +0200

    Removed: - Unused Test Class

--- src/test/java/de/darkatra/patcher/PatchController.java
+++
@@ -1,7 +0,0 @@
-package de.darkatra.patcher;
-
-import de.darkatra.patcher.listener.PatchEventListener;
-
-public class PatchController {
-	public void patch(PatchEventListener patchEventListener) {}
-}
