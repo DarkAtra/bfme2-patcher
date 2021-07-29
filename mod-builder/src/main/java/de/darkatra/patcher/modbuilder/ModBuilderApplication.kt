9180ab8b29fdfd880bc2b818c040aa773d62d834
DarkAtra <darkatra@web.de>
2021-07-29 17:55:47 +0200

    use the bfme2 modding utils lib, add logs

--- mod-builder/src/main/java/de/darkatra/patcher/modbuilder/ModBuilderApplication.kt
+++ mod-builder/src/main/java/de/darkatra/patcher/modbuilder/ModBuilderApplication.kt
@@ -1,9 +1,11 @@
 package de.darkatra.patcher.modbuilder
 
-import de.darkatra.patcher.modbuilder.model.BigArchive
+import de.darkatra.bfme2.big.BigArchive
+import de.darkatra.bfme2.big.BigArchiveVersion
 import java.nio.file.Files
 import java.nio.file.Path
 import java.util.stream.Collectors
+import kotlin.io.path.pathString
 
 const val OUTPUT_FILE_NAME = "update-builder/rotwk/!mod.big"
 const val OUTPUT_FILE_LANG_EN_NAME = "update-builder/rotwk/lang/englishstringsmod.big"
@@ -14,6 +16,7 @@ const val GERMAN_TRANSLATION_FILE = "data/german-lotr.str"
 const val LOTR_STR_NAME = "data/lotr.str"
 
 class ModBuilderApplication {
+
 	companion object {
 		@JvmStatic
 		fun main(args: Array<String>) {
@@ -21,37 +24,52 @@ class ModBuilderApplication {
 		}
 	}
 
-	private val excludedDirs = listOf(".git", ENGLISH_TRANSLATION_FILE, GERMAN_TRANSLATION_FILE)
+	private val excludedDirs = listOf(".git", ".gitignore", ".idea", ENGLISH_TRANSLATION_FILE, GERMAN_TRANSLATION_FILE)
 
 	fun build() {
+
+		println("Building mod archives...")
+
+		val inputDir = Path.of(MOD_DIR)
+		println("Reading files from: ${inputDir.pathString}")
 
 		val outFile = Path.of(OUTPUT_FILE_NAME).toFile()
 		outFile.mkdirs()
 		outFile.delete()
 
-		val inputDir = Path.of(MOD_DIR)
-
-		val bigArchive = BigArchive(outFile.outputStream())
+		println("* Main archive: ${outFile.path}")
+		val bigArchive = BigArchive(BigArchiveVersion.BIG_F)
 		readFilesInDirectory(inputDir)
 			.filter { file -> excludedDirs.none { prefix -> file.startsWith("$MOD_DIR/$prefix") } }
 			.forEach { file ->
+				println("** Adding file to archive: ${inputDir.relativize(file).pathString}")
 				bigArchive.addFile(file, inputDir.relativize(file).toString())
 			}
-		bigArchive.writeToDisk()
+
+		bigArchive.write(outFile.outputStream())
+		println("** Completed Main archive!")
 
 		buildTranslation(inputDir, ENGLISH_TRANSLATION_FILE, OUTPUT_FILE_LANG_EN_NAME)
 		buildTranslation(inputDir, GERMAN_TRANSLATION_FILE, OUTPUT_FILE_LANG_DE_NAME)
+
+		println("Success! Output: ${outFile.path}")
 	}
 
 	private fun buildTranslation(inputDir: Path, translationFile: String, outputFile: String) {
-		val germanTranslationFile = inputDir.resolve(translationFile)
+
+		val resolvedTranslationFile = inputDir.resolve(translationFile)
+		println("* Translation archive: ${resolvedTranslationFile.pathString}")
 		val outFile = Path.of(outputFile).toFile()
 		outFile.mkdirs()
 		outFile.delete()
 
-		BigArchive(outFile.outputStream()).also {
-			it.addFile(germanTranslationFile, LOTR_STR_NAME)
-		}.writeToDisk()
+		BigArchive(BigArchiveVersion.BIG_F).also {
+			println("** Adding file to archive: ${resolvedTranslationFile.pathString}")
+			it.addFile(resolvedTranslationFile, LOTR_STR_NAME)
+		}.also {
+			it.write(outFile.outputStream())
+		}
+		println("** Completed Translation archive!")
 	}
 
 	private fun readFilesInDirectory(directory: Path): Set<Path> {
--- mod-builder/src/main/java/de/darkatra/patcher/modbuilder/model/BigArchiveVersion.kt
+++
@@ -1,6 +0,0 @@
-package de.darkatra.patcher.modbuilder.model
-
-enum class BigArchiveVersion {
-	BIG_F,
-	BIG_4
-}
