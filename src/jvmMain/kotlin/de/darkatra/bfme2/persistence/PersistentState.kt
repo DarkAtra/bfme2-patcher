092099b6f7341b90b6bf282005f1830f1605232b
DarkAtra <darkatra@web.de>
2022-09-23 17:34:36 +0200

    fix: ignore unknown properties from previous patcher-state.json

--- src/jvmMain/kotlin/de/darkatra/bfme2/persistence/PersistentState.kt
+++ src/jvmMain/kotlin/de/darkatra/bfme2/persistence/PersistentState.kt
@@ -1,5 +1,8 @@
 package de.darkatra.bfme2.persistence
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class PersistentState(
     val hdEditionEnabled: Boolean = false,
     val trayIconEnabled: Boolean = false
