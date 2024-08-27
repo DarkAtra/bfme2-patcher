3809f8f0980cdf08c63c80f7b8210e2df25ad9f9
Tommy Schmidt <tommy.schmidt@idealo.de>
2024-08-27 23:49:59 +0200

    chore: remove github packages as dependency source

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,10 @@
 allprojects {
     group = "de.darkatra.bfme2"
-    version = "0.17.0"
+    version = "0.17.1"
 
     repositories {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.pkg.github.com/DarkAtra/*") {
-            credentials {
-                username = "${extra["github.username"]}"
-                password = "${extra["github.password"]}"
-            }
-        }
         google()
         mavenLocal()
     }
