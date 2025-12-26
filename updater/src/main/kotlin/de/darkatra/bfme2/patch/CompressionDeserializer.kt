753dc8969e1231b0f026cb0f4974cafdd8364901
DarkAtra <darkatra@gmail.com>
2025-12-26 23:16:58 +0100

    feat(graalvm): native application runs and patches files successfully
    Most of the application works as expected but `NativeImageUtils` still needs to be cleaned up and the `SelfUpdateService` also needs a rewrite since the native executable also requires some dlls to be downloaded and the current implementations can only handle a single file.

--- updater/src/main/kotlin/de/darkatra/bfme2/patch/CompressionDeserializer.kt
+++ updater/src/main/kotlin/de/darkatra/bfme2/patch/CompressionDeserializer.kt
@@ -1,7 +1,9 @@
 package de.darkatra.bfme2.patch
 
 import com.fasterxml.jackson.databind.util.StdConverter
+import io.goodforgod.graalvm.hint.annotation.ReflectionHint
 
+@ReflectionHint(ReflectionHint.AccessType.ALL_DECLARED_CONSTRUCTORS)
 class CompressionDeserializer : StdConverter<String, Compression>() {
 
     override fun convert(value: String?): Compression? {
